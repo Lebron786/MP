a7a7671f435ea8c244bf2ed04b4eab814e0d1f05
Lebron786 <abdulmpkos@gmail.com>
2025-03-06 17:47:22 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript to trigger additional animations or interactions
-document.addEventListener('DOMContentLoaded', function() {
-    // You can add more animations or effects here if you'd like
-    const heart = document.querySelector('.heart');
-    heart.addEventListener('animationiteration', () => {
-        console.log("Heart animation iteration!");
-    });
-});
